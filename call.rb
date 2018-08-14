2de0270186f3a8051f1fa3ae4495c557cc224036
reiji <i.can.fly1005@ezweb.ne.jp>
2018-08-15 05:41:47 +0900

    [add]通話の発信

--- call.rb
+++ call.rb
@@ -4,3 +4,13 @@ require 'twilio-ruby'
 
 account_sid = ENV['ACCOUNT_SID']
 auth_token = ENV['AUTH_TOKEN']
+
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+call = @client.calls.create(
+  url: 'http://demo.twilio.com/docs/voice.xml',
+  to: ENV['CALL_TO'],
+  from: ENV['CALL_FROM']
+  )
+
+puts call.sid
