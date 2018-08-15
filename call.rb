c6881fe30ffd13cc2db28d7eddd3a47b6f377f82
reiji <i.can.fly1005@ezweb.ne.jp>
2018-08-16 03:34:32 +0900

    [fix]update file

--- call.rb
+++ call.rb
@@ -7,10 +7,10 @@ auth_token = ENV['AUTH_TOKEN']
 
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
+# 通話の発信
 call = @client.calls.create(
   url: 'http://demo.twilio.com/docs/voice.xml',
   to: ENV['CALL_TO'],
   from: ENV['CALL_FROM']
   )
-
 puts call.sid
